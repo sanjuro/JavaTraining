68d28028b4e40aa870f83a7b8de04bed2988537a
Shadley <shadley@Damiens-MacBook-Pro.local>
2016-07-21 09:54:53 +0200

    Training projects

---
+++ RaceManagerGUI/src/raceManagerData/dataFile.java
@@ -0,0 +1,10 @@
+package raceManagerData;
+
+public class dataFile 
+{
+	public static String dataFilePath = "/Users/shadley/Development/Training/Java/Java 8 - Beyond the Basics - Working Files/Chapter 8/RaceManagerGUI"
+			+ "/src/raceManagerData/raceManagerData.txt";
+
+	public static String dataObjectFilePath = "/Users/shadley/Development/Training/Java/Java 8 - Beyond the Basics - Working Files/Chapter 8/RaceManagerGUI"
+			+ "/src/raceManagerData/raceManagerData.bin";
+}
